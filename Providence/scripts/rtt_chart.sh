4dfe7d50ac61b1cd65956c387fd7626c56d1a117
Sean Whitney <75866669+swhitney-appneta@users.noreply.github.com>
2023-12-01 09:46:10 -0800

    modified script to generate nontimestamped csv files, and changed to 2hour (at the top of the hour) rtt averages

--- Providence/scripts/rtt_chart.sh
+++ Providence/scripts/rtt_chart.sh
@@ -20,10 +20,10 @@
 
 SL="$(which sqlite3) -batch -init /dev/null "
 LOCAL_DB="../sql/api.sqlt"
-# By default the database will contain 30 days of data, but only reporting on the
-# last week (based on the top of the last hour).  If there is a need to store less
-# data then change the variable OLDDATE (currently 60sec*60min*24hours*30days)
-OLDDATE=2592000
+# By default the database will contain  days of data, but only reporting on the
+# last 2 hours (based on the top of the last hour).  If there is a need to store less
+# data then change the variable OLDDATE (currently 60sec*60min*24hours*7days)
+OLDDATE=604800
 
 #######################################################################################
 # Functions                                                                           #
@@ -47,6 +47,7 @@ delete_views() {
 	SQL="DROP VIEW IF EXISTS appliance_ip_not_n10_view;
          DROP VIEW IF EXISTS rtt_target_id_view;
          DROP VIEW IF EXISTS rtt_path_id_view;
+         DROP VIEW IF EXISTS rtt_2h_avg_view;
          DROP VIEW IF EXISTS rtt_weekly_avg_view;
          DROP VIEW IF EXISTS rtt_results_view;
          DROP VIEW IF EXISTS rtt_raw_results_view;
@@ -63,7 +64,7 @@ info() {
 	echo
 	echo "$FN creates several views and uses them to create a view with rtt times between"
 	echo "selected monitoring points. This script can take a long time to run, but should"
-	echo "complete within a few hours."
+	echo "complete within 1 hour."
 	echo "Usage: $FN [-dh]"
 	echo "  -d  deletes existing views created by this script and exits.  The script will"
 	echo "      automatically recreate these views the next time it runs "
@@ -147,13 +148,13 @@ $SL "$LOCAL_DB" "$SQL"
 
 # This isn't really needed but doesn't take and resources to create.  PowerBI
 # importing might be easier with this view
-# Create rtt_weekly_avg_view if not created
-SQL="CREATE VIEW IF NOT EXISTS rtt_weekly_avg_view AS
+# Create rtt_2h_avg_view if not created
+SQL="CREATE VIEW IF NOT EXISTS rtt_2h_avg_view AS
        SELECT pathId, round(avg(value),1) AS rtt_avg
        FROM pathdata_data 
        WHERE type='rtt' AND 
        start BETWEEN
-       ((STRFTIME('%s','now')/3600)*3600-86400)*1000 AND 
+       ((STRFTIME('%s','now')/3600)*3600-7200)*1000 AND 
        (STRFTIME('%s','now')/3600)*3600*1000 
        GROUP BY pathId"
 $SL "$LOCAL_DB" "$SQL"
@@ -163,7 +164,7 @@ SQL="CREATE VIEW IF NOT EXISTS rtt_results_view AS
        SELECT pathId, value AS rtt 
        FROM pathdata_data 
        WHERE type='rtt' 
-       AND start BETWEEN ((STRFTIME('%s','now')/3600)*3600-86400)*1000 
+       AND start BETWEEN ((STRFTIME('%s','now')/3600)*3600-7200)*1000 
        AND (STRFTIME('%s','now')/3600)*3600*1000"
 $SL "$LOCAL_DB" "$SQL"
 
@@ -278,11 +279,11 @@ $SL "$LOCAL_DB" "$SQL"
 #######################################################################################
 # Output the results to a dated file                                                  #
 #######################################################################################
-TS=$(date '+%Y%m%d%H')
+#TS=$(date '+%Y%m%d%H')
 
 $SL "$LOCAL_DB" <<EOF
 .mode csv
 .headers on
-.output ${TS}rtt_table.csv
+.output rtt_table.csv
 select * from rtt_table;
 EOF
